b667061f96f007d9fe188e21a370102a3ea9153c
Roulian <tijujudu54@hotmail.fr>
2019-11-20 09:50:13 +0100

    correction du script

--- script/insert-album.js
+++ script/insert-album.js
@@ -1,117 +1,141 @@
 // Insert album array
-db.getCollection('album').insertMany([
+db.getCollection('albums').insertMany([
     {
-        titre: 'un magnifique album',
-        photo: 'https://www.crealitiz.com/19022-large_default/couverture-mon-album.jpg',
-        description: 'Un album rempli de beaux cliches',
-        auteur: 'Julien Bailly',
-        photo0: {
-            titre: 'photo0',
-            path: 'https://www.naturimages.com/imgrsz/5c5bcccc000000000000000026428ca07ac91b253eb7a03239c669c1.jpg/bg-5c5bcccc0000000000000000.jpg',
-            description: 'belle nature',
-            auteur: 'Inconnu',
+        "titre": "un magnifique album",
+        "photo": "https://www.crealitiz.com/19022-large_default/couverture-mon-album.jpg",
+        "description": "Un album rempli de beaux cliches",
+        "auteur": "Julien Bailly",
+        "photo0": {
+            "titre": "photo0",
+            "path": "https://www.naturimages.com/imgrsz/5c5bcccc000000000000000026428ca07ac91b253eb7a03239c669c1.jpg/bg-5c5bcccc0000000000000000.jpg",
+            "description": "belle nature",
+            "auteur": "Inconnu",
         },
-        photo1: {
-            titre: 'photo1',
-            path: 'https://images.unsplash.com/photo-1532465614-6cc8d45f647f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80',
-            description: 'belle nature',
-            auteur: 'Inconnu',
+        "photo1": {
+            "titre": "photo1",
+            "path": "https://images.unsplash.com/photo-1532465614-6cc8d45f647f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80",
+            "description": "belle nature",
+            "auteur": "Inconnu",
         },
-        photo2:  {
-            titre: 'photo2',
-            path: 'https://images.unsplash.com/photo-1532465614-6cc8d45f647f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80',
-            description: 'belle nature',
-            auteur: 'Inconnu',
+        "photo2":  {
+            "titre": "photo2",
+            "path": "https://images.unsplash.com/photo-1532465614-6cc8d45f647f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80",
+            "description": "belle nature",
+            "auteur": "Inconnu",
         },
-        photo3: {
-            titre: 'photo3',
-            path: 'https://www.iphon.fr/app/uploads/2019/09/test-photo-iphone-11-pro-austin-mann-0.jpg',
-            description: 'belle nature',
-            auteur: 'Inconnu',
+        "photo3": {
+            "titre": "photo3",
+            "path": "https://www.iphon.fr/app/uploads/2019/09/test-photo-iphone-11-pro-austin-mann-0.jpg",
+            "description": "belle nature",
+            "auteur": "Inconnu",
         },
     },
     {
-        titre: 'un magnifique album',
-        photo: 'https://www.crealitiz.com/19022-large_default/couverture-mon-album.jpg',
-        description: 'Un album rempli de choses',
-        auteur: 'Julien Bailly',
-        photo0: {
-            titre: 'photo0',
-            path: 'https://s3-eu-west-1.amazonaws.com/sc-files.pjms.fr/p/pjms/383/000/402/763/a839d5889920446e8d6801ddacda7f99.jpg',
-            description: 'belle chose',
-            auteur: 'Inconnu',
+        "titre": "un magnifique album",
+        "photo": "https://images-na.ssl-images-amazon.com/images/I/71YWmrhVCCL._SX425_.jpg",
+        "description": "Un album rempli de choses",
+        "auteur": "Julien Bailly",
+        "photo0": {
+            "titre": "photo0",
+            "path": "https://s3-eu-west-1.amazonaws.com/sc-files.pjms.fr/p/pjms/383/000/402/763/a839d5889920446e8d6801ddacda7f99.jpg",
+            "description": "belle chose",
+            "auteur": "Inconnu",
         },
-        photo1: {
-            titre: 'photo1',
-            path: 'http://tontonphoto.fr/wp-content/uploads/2018/06/test-labophoto-tirage-art-paysage-09-tonton-photo.jpg',
-            description: 'belle vue',
-            auteur: 'Tonton Photo',
+        "photo1": {
+            "titre": "photo1",
+            "path": "http://tontonphoto.fr/wp-content/uploads/2018/06/test-labophoto-tirage-art-paysage-09-tonton-photo.jpg",
+            "description": "belle vue",
+            "auteur": "Tonton Photo",
         },
     },
     {
-        titre: "Paysages",
-        photo: "https://randomuser.me/portraits/men/55.jpg",
-        description: "Contient de belles photos",
-        auteur: "Julien BAILLY",
-        photo0: {
-            titre: "Chat.jpg",
-            path: "https://randomuser.me/portraits/men/55.jpg",
-            description: "Photo de chat",
-            auteur: "Julien BAILLY"
+        "titre": "Album exemple",
+        "photo": "https://randomuser.me/portraits/men/55.jpg",
+        "description": "Contient de belles photos",
+        "auteur": "Julien BAILLY",
+        "photo0": {
+            "titre": "Chat.jpg",
+            "path": "https://randomuser.me/portraits/men/55.jpg",
+            "description": "Photo de chat",
+            "auteur": "Julien BAILLY"
         },
-        photo1: {
-            titre: "Chat.jpg",
-            path: "https://randomuser.me/portraits/men/55.jpg",
-            description: "Photo de chat",
-            auteur: "Julien BAILLY"
+        "photo1": {
+            "titre": "Chat.jpg",
+            "path": "https://randomuser.me/portraits/men/55.jpg",
+            "description": "Photo de chat",
+            "auteur": "Julien BAILLY"
         },
-        photo2: {
-            titre: "Chat.jpg",
-            path: "https://randomuser.me/portraits/men/55.jpg",
-            description: "Photo de chat",
-            auteur: "Julien BAILLY"
+        "photo2": {
+            "titre": "Chat.jpg",
+            "path": "https://randomuser.me/portraits/men/55.jpg",
+            "description": "Photo de chat",
+            "auteur": "Julien BAILLY"
         },
-        photo3: {
-            titre: "Chat.jpg",
-            path: "https://randomuser.me/portraits/men/55.jpg",
-            description: "Photo de chat",
-            auteur: "Julien BAILLY"
+        "photo3": {
+            "titre": "Chat.jpg",
+            "path": "https://randomuser.me/portraits/men/55.jpg",
+            "description": "Photo de chat",
+            "auteur": "Julien BAILLY"
         },
-        photo4: {
-            titre: "Chat.jpg",
-            path: "https://randomuser.me/portraits/men/55.jpg",
-            description: "Photo de chat",
-            auteur: "Julien BAILLY"
+        "photo4": {
+            "titre": "Chat.jpg",
+            "path": "https://randomuser.me/portraits/men/55.jpg",
+            "description": "Photo de chat",
+            "auteur": "Julien BAILLY"
         },
-        photo5: {
-            titre: "Chat.jpg",
-            path: "https://randomuser.me/portraits/men/55.jpg",
-            description: "Photo de chat",
-            auteur: "Julien BAILLY"
+        "photo5": {
+            "titre": "Chat.jpg",
+            "path": "https://randomuser.me/portraits/men/55.jpg",
+            "description": "Photo de chat",
+            "auteur": "Julien BAILLY"
         },
-        photo6: {
-            titre: "Chat.jpg",
-            path: "https://randomuser.me/portraits/men/55.jpg",
-            description: "Photo de chat",
-            auteur: "Julien BAILLY"
+        "photo6": {
+            "titre": "Chat.jpg",
+            "path": "https://randomuser.me/portraits/men/55.jpg",
+            "description": "Photo de chat",
+            "auteur": "Julien BAILLY"
         },
-        photo7: {
-            titre: "Chat.jpg",
-            path: "https://randomuser.me/portraits/men/55.jpg",
-            description: "Photo de chat",
-            auteur: "Julien BAILLY"
+        "photo7": {
+            "titre": "Chat.jpg",
+            "path": "https://randomuser.me/portraits/men/55.jpg",
+            "description": "Photo de chat",
+            "auteur": "Julien BAILLY"
         },
-        photo8: {
-            titre: "Chat.jpg",
-            path: "https://randomuser.me/portraits/men/55.jpg",
-            description: "Photo de chat",
-            auteur: "Julien BAILLY"
+        "photo8": {
+            "titre": "Chat.jpg",
+            "path": "https://randomuser.me/portraits/men/55.jpg",
+            "description": "Photo de chat",
+            "auteur": "Julien BAILLY"
         },
-        photo9: {
-            titre: "Chat.jpg",
-            path: "https://randomuser.me/portraits/men/55.jpg",
-            description: "Photo de chat",
-            auteur: "Julien BAILLY"
+        "photo9": {
+            "titre": "Chat.jpg",
+            "path": "https://randomuser.me/portraits/men/55.jpg",
+            "description": "Photo de chat",
+            "auteur": "Julien BAILLY"
         }
+    },
+    {
+        "titre": "Islande",
+        "photo": "http://www.nova.fr/sites/default/files/styles/ratio_2_5_xl/public/articles/images/phpthumb_generated_thumbnail.jpeg?itok=GsClS0sB",
+        "description": "Les magnifiques paysages islandais",
+        "auteur": "Julien BAILLY",
+        "photo0": {
+            "titre": "Chute d\"eau",
+            "path": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQhOD4LjQDpq6o42QiQsTwNnRWeMKZOrcW-6aZGlANzFvRyzJn&spg",
+            "description": "",
+            "auteur": "Julien BAILLY",
+        },
+        "photo1": {
+            "titre": "Plage de sable noir",
+            "path": "https://www.voyageursdumonde.fr/voyage-sur-mesure/magazine-voyage/ShowPhoto/490/0",
+            "description": "Plage avec montagne en fond",
+            "auteur": "Julien BAILLY",
+        },
+        "photo2": {
+            "titre": "Blue lagoon",
+            "path": "https://resize-parismatch.lanmedia.fr/r/625,417,forcex,center-middle/img/var/news/storage/images/paris-match/vivre/voyage/islande-le-grand-frisson-1068985/15278407-1-fre-FR/Islande-le-grand-frisson.jpg",
+            "description": "Eau très chaude",
+            "auteur": "Julien BAILLY",
+        },
     }
 ]);
